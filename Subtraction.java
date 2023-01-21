348ad705991b9d76d87f0987d17a6ec9e24de94b
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-01-21 14:10:12 +0530

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,13 @@
+class Subtraction
+{
+public static void main(String args[])
+{
+System.out.println("this first number is:");
+int a=5;
+int b=6;
+System.out.println(a);
+System.out.println("this second number is:");
+System.out.println(b);
+int sub=a-b;
+System.out.println("the sub is ="+sub);
+}}
