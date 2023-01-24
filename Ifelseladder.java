c3b4356c4baa466ffc882ccc7f09c6e3b4e71c86
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-01-24 19:38:51 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,21 @@
+class Ifelseladder
+{
+public static void main(String args[])
+{
+String name="Riya";
+if(name=="Meena")
+{
+System.out.println("name is Meena");
+}
+else if(name=="Neha")
+{
+System.out.println("name is Neha");
+}
+else if(name=="Amit")
+{
+System.out.println("name is Amit");
+}
+else
+{
+System.out.println(name);
+}}}
