12a75289bff3755c15e3f24ad264e70e5040c2ce
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-02-03 14:35:45 +0530

    Add files via upload

---
+++ SwitchEg2.java
@@ -0,0 +1,27 @@
+package controlSystem;
+
+public class SwitchEg2 {
+	public static void main(String[] args) {
+int no=10;
+switch(no)
+{
+case 1:
+	System.out.println("20");
+break;
+case 2:
+	System.out.println("10");
+break;
+case 3:
+	System.out.println("40");
+break;
+case 10:
+	System.out.println("50");
+break;
+
+default:System.out.println("invalid number");
+}
+	}
+}
+
+
+
