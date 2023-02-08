afb5552e71dd97e351a8c089e5216f774476791b
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-02-08 12:57:14 +0530

    Add files via upload

---
+++ Dowhile3.java
@@ -0,0 +1,15 @@
+package controlSystem;
+
+public class Dowhile3 {
+	public static void main(String[] args) {
+		
+		int i=2;
+		System.out.println("First 10 even number");
+		do {
+			System.out.println(i);
+			i=i+2;
+		}while(i<=10);
+
+		}
+
+}
