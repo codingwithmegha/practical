3a704f0505a2f09fec24179fd271c805a3b41e3e
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-02-10 21:54:20 +0530

    Add files via upload

---
+++ double_variabletest.java
@@ -0,0 +1,13 @@
+package Method_program;
+import java.util.Scanner;
+public class double_variabletest {
+		public static void main(String[] args) {
+			 Scanner sc=new Scanner(System.in);
+			 System.out.print("Input first number: ");
+	         double n=sc.nextDouble();
+	         System.out.print("Input second number: ");
+	         double m=sc.nextDouble();
+			 System.out.println(n>0 && m<1 && m>0 && m<1);
+		}
+	}
+
