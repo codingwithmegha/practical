ccd63422a4607d24dcf899f90586e5f55ceb3d25
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-02-18 20:19:18 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class Address {  //another class
+String city,state,country;
+public Address(String city,String state,String country) {
+	this.city=city;
+	this.state=state;
+	this.country=country;
+}
+}
