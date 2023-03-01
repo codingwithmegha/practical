8874ba6791a69b54e106060b4e07375347b78a04
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-03-01 16:18:21 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,33 @@
+package mypackage;
+
+public class Bank {
+private long Acount_no;
+private String Name;
+private String Email;
+private float Amount;
+public long getAcount_no() {
+	return Acount_no;
+}
+public void setAcount_no(long acount_no) {
+	Acount_no = acount_no;
+}
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+public String getEmail() {
+	return Email;
+}
+public void setEmail(String email) {
+	Email = email;
+}
+public float getAmount() {
+	return Amount;
+}
+public void setAmount(float amount) {
+	Amount = amount;
+}
+}
+
