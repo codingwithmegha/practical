4d7862c1fc17827ae7b29aa6f7bdb1bfb3959283
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-03-10 16:14:30 +0530

    Add files via upload

---
+++ copy_array_no.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class copy_array_no {
+	public static void main(String[] args) {
+	double arr[]={9,2,5,3,8}; //Initialition and declaration ofarray       
+    //getting the class name
+Class a=arr.getClass();
+String name=a.getName();
+//print the class name
+System.out.println(a);
+}
+}
