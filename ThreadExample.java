ad44a6de7cdcdf657eef72356b224b4363be85f1
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-03-29 14:34:16 +0530

    Add files via upload

---
+++ ThreadExample.java
@@ -0,0 +1,15 @@
+package Thread;
+//creating thread by extending thread class
+public class ThreadExample extends Thread{
+public void run() {
+	System.out.println("I am Thread....thread is running");
+}
+public static void main(String[] args) {
+	ThreadExample t=new ThreadExample();
+	t.start();  //move the thread to the active state
+	t.start();
+//t.setName("megha");
+//String s=t.getName();
+//System.out.println(s);
+}
+}
