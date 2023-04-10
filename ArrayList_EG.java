70e19eb24aef68d94da415aa4dfcb5e7dddd13ad
codingwithmegha <122961155+codingwithmegha@users.noreply.github.com>
2023-04-10 22:18:24 +0530

    Add files via upload

---
+++ ArrayList_EG.java
@@ -0,0 +1,16 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class ArrayList_EG {//main class
+public static void main(String[] args) {//main method
+ArrayList<String> al=new ArrayList<String>();//creating array list
+//all element in arraylist through object
+al.add("sana");
+al.add("ritu");
+al.add("upsana");
+al.add("megha");
+//printing array list
+System.out.println(al);
+}
+}
